e3f2a3678784010e6e1ce5c6cd6ee3e58a048381
Jeffrey Patey <jeffreyvpatey@gmail.com>
2026-02-14 13:10:33 -0400

    added user menu to canvas toolbar

--- app/projects/[id]/page.tsx
+++ app/projects/[id]/page.tsx
@@ -477,13 +477,11 @@ export default function ProjectCanvasPage() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-slate-50 via-white to-slate-50 dark:from-slate-900 dark:via-slate-800 dark:to-slate-900 overflow-hidden">
       {/* Canvas Header */}
-      <CanvasHeader
-        project={project}
-        viewMode={viewMode}
-        onViewModeChange={setViewMode}
-        onSidebarToggle={() => setSidebarOpen(!sidebarOpen)}
-        sidebarOpen={sidebarOpen}
-      />
+        <CanvasHeader
+          project={project}
+          viewMode={viewMode}
+          onViewModeChange={setViewMode}
+        />
       {/* Main Canvas Layout */}
       <div className="flex h-[calc(100vh-64px)] relative">
         {/* Floating "Show sidebar" tab when sidebar is closed */}
